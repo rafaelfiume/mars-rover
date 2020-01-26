1c0b39f062be888af25fb4741bb44c2596da1de5
Rafael Fiume <rafael.fiume@yahoo.it>
2020-01-26 13:07:21 +0000

    Search the shortest path between two points (without any obstacles)

--- src/main/scala/rover/Grid.scala
+++ src/main/scala/rover/Grid.scala
@@ -1,5 +1,7 @@
 package rover
 
+import scala.annotation.tailrec
+
 object Grid {
 
   type Node = (Int, Int)
@@ -14,4 +16,42 @@ object Grid {
       down    = (if (r+1 >= rows) 0 else r+1, c)
       right = (r, if (c+1 >= columns) 0 else c+1)
     } yield (r, c) -> List(left, up, down, right)
+
+  def shortestPath(start: Node, end: Node, grid: Grid): List[Node] = {
+    type Parents = Map[Node, Node]
+
+    def pathTo(node: Node, through: Parents): List[Node] = through.get(node) match {
+      case None => List.empty
+      case Some(parent) => parent :: pathTo(parent, through)
+    }
+
+    def nodesParent(nodes: List[Node], parent: Node): Parents = {
+      nodes.foldLeft(Map.empty[Node, Node]) { case (parents, n) => parents.updated(n, parent) }
+    }
+
+    @tailrec
+    def bdf(searched: List[Node],
+            queue: List[Node],
+            parents: Parents): Parents = queue match {
+      case Nil => parents
+      case x :: _ if x == end => parents
+      case x :: xs =>
+        val updatedSearched = x :: searched
+        val nodeNeighbours = neighbours(x, grid).filterNot(n => updatedSearched.contains(n) || queue.contains(n))
+        val neighboursParent = nodesParent(nodeNeighbours, parent = x)
+        bdf(updatedSearched, xs ++ nodeNeighbours, parents ++ neighboursParent)
+    }
+
+    val neighboursFromStart = neighbours(start, grid)
+    val startNeighboursParent = nodesParent(neighboursFromStart, parent = start)
+
+    val parents = bdf(List(start), neighboursFromStart, startNeighboursParent)
+    val pathToEnd = pathTo(end, through = parents)
+    if (pathToEnd.isEmpty) List.empty else (end :: pathToEnd).reverse
+  }
+
+  def neighbours(node: Node, grid: Grid): List[Node] = grid
+    .find { case (n, _) => n == node }
+    .map (_._2)
+    .getOrElse (List.empty)
 }
--- src/test/scala/rover/GridSpec.scala
+++ src/test/scala/rover/GridSpec.scala
@@ -27,4 +27,22 @@ Rover will always try to turn 'left' first, then 'up', 'down' and, finally, 'rig
       (2,0) -> List((2,4), (1,0), (0,0), (2,1)), (2,1) -> List((2,0), (1,1), (0,1), (2,2)), (2,2) -> List((2,1), (1,2), (0,2), (2,3)), (2,3) -> List((2,2), (1,3), (0,3), (2,4)), (2,4) -> List((2,3), (1,4), (0,4), (2,0))
     )
   }
+
+  "shortestPath" should "find the shortest path from one point of the grid to the other" in {
+    shortestPath(start = (0,0), end = (2,4), aGrid) shouldBe List((0,0), (0,4), (2,4))
+    shortestPath(start = (1,4), end = (0,1), aGrid) shouldBe List((1,4), (0,4), (0,0), (0,1))
+    shortestPath(start = (2,0), end = (1,1), aGrid) shouldBe List((2,0), (1,0), (1,1))
+    shortestPath(start = (2,4), end = (2,1), aGrid) shouldBe List((2,4), (2,0), (2,1))
+    shortestPath(start = (2,2), end = (0,4), aGrid) shouldBe List((2,2), (0,2), (0,3), (0,4))
+  }
+
+  it should "return an empty path when there's no path to the target" in {
+    shortestPath(start = (0,0), end = (8,8), aGrid) shouldBe empty
+    shortestPath(start = (8,8), end = (0,0), aGrid) shouldBe empty
+  }
+
+  "neighbours" should "return all the neighbours of a node" in {
+    neighbours((1,2), aGrid) shouldBe List((1,1), (0,2), (2,2), (1,3))
+  }
+
 }
