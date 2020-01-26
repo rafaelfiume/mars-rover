1a0c4427d539dcce05b1d52ffb79a99d8b5e5be3
Rafael Fiume <rafael.fiume@yahoo.it>
2020-01-26 13:39:50 +0000

    Add option to create obstacles in a grid

--- src/main/scala/rover/Grid.scala
+++ src/main/scala/rover/Grid.scala
@@ -1,6 +1,15 @@
 package rover
 
+import rover.Grid.{ Edges, Node }
+
 import scala.annotation.tailrec
+
+object Edges {
+  def apply(edges: Edges, obstacles: Option[List[Node]]): Edges = obstacles match {
+    case None => edges
+    case Some(obs) => edges.filterNot(obs.contains(_))
+  }
+}
 
 object Grid {
 
@@ -8,14 +17,15 @@ object Grid {
   type Edges = List[Node]
   type Grid = Seq[(Node, Edges)]
 
-  def apply(rows: Int, columns: Int): Grid = for {
+  def apply(rows: Int, columns: Int, obstacles: Option[List[Node]] = None): Grid = for {
       r     <- 0 until rows
       c     <- 0 until columns
+      if !obstacles.exists(_.contains((r,c)))
       left  = (r, if (c-1 < 0) columns-1 else c-1)
       up    = (if (r-1 < 0) rows-1 else r-1, c)
       down    = (if (r+1 >= rows) 0 else r+1, c)
       right = (r, if (c+1 >= columns) 0 else c+1)
-    } yield (r, c) -> List(left, up, down, right)
+    } yield (r, c) -> Edges(List(left, up, down, right), obstacles)
 
   def shortestPath(start: Node, end: Node, grid: Grid): List[Node] = {
     type Parents = Map[Node, Node]
--- src/test/scala/rover/GridSpec.scala
+++ src/test/scala/rover/GridSpec.scala
@@ -16,15 +16,34 @@ Rover will always try to turn 'left' first, then 'up', 'down' and, finally, 'rig
 |-------|-------|-------|-------|-------|
 | (2,0) | (2,1) | (2,2) | (2,3) | (2,4) |
 |-------|-------|-------|-------|-------|
-
   """ }
-  private val aGrid = Grid(3, 5)
+  private val aGrid = Grid(rows = 3, columns = 5)
 
   "Grid apply" should "create a grid (adjacency list) with R x C size" in {
     aGrid shouldBe Vector(
       (0,0) -> List((0,4), (2,0), (1,0), (0,1)), (0,1) -> List((0,0), (2,1), (1,1), (0,2)), (0,2) -> List((0,1), (2,2), (1,2), (0,3)), (0,3) -> List((0,2), (2,3), (1,3), (0,4)), (0,4) -> List((0,3), (2,4), (1,4), (0,0)),
       (1,0) -> List((1,4), (0,0), (2,0), (1,1)), (1,1) -> List((1,0), (0,1), (2,1), (1,2)), (1,2) -> List((1,1), (0,2), (2,2), (1,3)), (1,3) -> List((1,2), (0,3), (2,3), (1,4)), (1,4) -> List((1,3), (0,4), (2,4), (1,0)),
       (2,0) -> List((2,4), (1,0), (0,0), (2,1)), (2,1) -> List((2,0), (1,1), (0,1), (2,2)), (2,2) -> List((2,1), (1,2), (0,2), (2,3)), (2,3) -> List((2,2), (1,3), (0,3), (2,4)), (2,4) -> List((2,3), (1,4), (0,4), (2,0))
+    )
+  }
+
+  markup {"""
+A Grid may have obstacles:
+            |-------|-------|-------|
+            | (0,0) | (0,1) | ##### |
+            |-------|-------|-------|
+            | (1,0) | (1,1) | ##### |
+            |-------|-------|-------|
+            | ##### | (2,1) | (2,2) |
+            |-------|-------|-------|
+  """ }
+  private val aGridWithObstacles = Grid(rows = 3, columns = 3, obstacles = Some(List((2,0), (0,2), (1,2))))
+
+  it should "have no connections where there are obstacles" in {
+    aGridWithObstacles shouldBe Vector(
+      (0,0) -> List((1,0), (0,1)), (0,1) -> List((0,0), (2,1), (1,1)),
+      (1,0) -> List((0,0), (1,1)), (1,1) -> List((1,0), (0,1), (2,1)),
+                                   (2,1) -> List((1,1), (0,1), (2,2)), (2,2) -> List((2,1))
     )
   }
 
@@ -39,6 +58,14 @@ Rover will always try to turn 'left' first, then 'up', 'down' and, finally, 'rig
   it should "return an empty path when there's no path to the target" in {
     shortestPath(start = (0,0), end = (8,8), aGrid) shouldBe empty
     shortestPath(start = (8,8), end = (0,0), aGrid) shouldBe empty
+  }
+
+  it should "find shortest distance when there are obstacles and the target node is reachable" in {
+    val grid = Grid(4, 3, Some(List((1,1), (1,2))))
+
+    val result = shortestPath(start = (2,2), end = (0,1), grid)
+
+    result shouldBe List((2,2), (2,1), (3,1), (0,1))
   }
 
   "neighbours" should "return all the neighbours of a node" in {
